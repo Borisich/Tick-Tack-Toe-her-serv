2a0410cf1ae04574678858481dc57342d335a2a3
Borisich <iv.xromov@mail.ru>
2016-12-13 18:18:06 +0300

    upd

--- server/index.js
+++ server/index.js
@@ -31,11 +31,12 @@ app.get('/', function(request, response) {
 });
 */
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
- });
+});
  
 app.get('/', function(request, response) {
   
