b4304ca513295c5ac1c2db512957dfed61b506d4
Borisich <iv.xromov@mail.ru>
2016-12-13 18:22:53 +0300

    res.headers2

--- server/index.js
+++ server/index.js
@@ -31,12 +31,34 @@ app.get('/', function(request, response) {
 });
 */
 
-app.all('*', function(req, res, next) {
+/*app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+ */
+ 
+ app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://mirt.spb.ru/ttt/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+ 
+ 
  
 app.get('/', function(request, response) {
   
