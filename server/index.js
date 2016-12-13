552c6004d5bb12217fb2ea85dd5f45769e108427
Borisich <iv.xromov@mail.ru>
2016-12-13 18:08:51 +0300

    res.headers

--- server/index.js
+++ server/index.js
@@ -31,7 +31,14 @@ app.get('/', function(request, response) {
 });
 */
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+ 
 app.get('/', function(request, response) {
+  
   response.send('Hello man');
 });
 
